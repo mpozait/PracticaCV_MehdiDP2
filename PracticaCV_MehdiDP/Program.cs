229c57196f4db8f1cb93d59956bf1512f0f0825b
Mehdi Del Pozo Ait el Hend <mpozait812@m.educaand.es>
2026-02-18 08:55:08 +0100

    Estructura del Programa de Mehdi_DP

--- PracticaCV_MehdiDP/Program.cs
+++ PracticaCV_MehdiDP/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //CONSTANTES
+
+            //VARIABLES
+
+            //INICIALIZACION
+
+            //ENTRADA
+
+            //PROCESO
+
+            //SALIDA
         }
     }
 }
